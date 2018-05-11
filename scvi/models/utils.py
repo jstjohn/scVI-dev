5a0ba5c63b8e38e170d7678edaaa316e1375aa7f
Edouard360 <mailedouardmehlman@gmail.com>
2018-05-11 13:33:21 -0700

    broadcast labels

--- scvi/models/utils.py
+++ scvi/models/utils.py
@@ -2,6 +2,26 @@ import torch
 from torch.distributions import register_kl, Multinomial
 
 
+def iterate(obj, func):
+    t = type(obj)
+    if t is list or t is tuple:
+        return t([iterate(o, func) for o in obj])
+    else:
+        return func(obj) if obj is not None else None
+
+
+def broadcast_labels(y, *o, n_broadcast=-1):
+    if not len(o):
+        raise "Broadcast must have at least one reference argument"
+    if y is None:
+        ys = enumerate_discrete(o[0], n_broadcast)
+        new_o = iterate(o, lambda x: x.repeat(n_broadcast, 1))
+    else:
+        ys = one_hot(y, n_broadcast)
+        new_o = o
+    return (ys,) + new_o
+
+
 def one_hot(index, n_cat):
     onehot = torch.zeros(index.size(0), n_cat, device=index.device)
     onehot.scatter_(1, index.type(torch.long), 1)
