06bc8e212d6801e936d71f3eea2cf3d356f83efa
Yining Liu <lyining@berkeley.edu>
2018-05-29 09:49:26 -0700

    subsample

--- scvi/dataset/cbmc.py
+++ scvi/dataset/cbmc.py
@@ -1,18 +1,42 @@
+import collections
 import csv
 import gzip
 import numpy as np
 
 from .dataset import GeneExpressionDataset
 
+GeneCbmcMatrix = collections.namedtuple('GeneCmbcMatrix', ['gene_ids', 'gene_names', 'expressions'])
+
+
+def subsample_barcodes(gbm, barcode_indices, unit_test=False):
+    barcodes = gbm.barcodes[barcode_indices] if not unit_test else gbm.barcodes
+    return GeneCbmcMatrix(gbm.gene_ids, gbm.gene_names, barcodes,
+                        gbm.matrix[:, barcode_indices])
+
+
+def subsample_genes(gbm, genes_indices, unit_test=False):
+    gene_ids = gbm.gene_ids[genes_indices] if not unit_test else gbm.gene_ids
+    gene_names = gbm.gene_names[genes_indices] if not unit_test else gbm.gene_names
+    return GeneCbmcMatrix(gene_ids, gene_names, gbm.barcodes,
+                        gbm.matrix[genes_indices, :])
+
 
 class CbmcDataset(GeneExpressionDataset):
     url = "https://www.ncbi.nlm.nih.gov/geo/download/" + \
               "?acc=GSE100866&format=file&file=GSE100866%5FCBMC%5F8K%5F13AB%5F10X%2DRNA%5Fumi%2Ecsv%2Egz"
 
-    def __init__(self):
-        # Generating samples according to a ZINB process
+    def __init__(self, subsample_size=None, unit_test=False, nb_genes_kept=720):
+        self.subsample_size = subsample_size if not unit_test else 128
         self.save_path = 'data/'
-        self.download_name = 'GSE100866_CBMC_8K_13AB_10X-RNA_umi.csv.gz'
+        self.unit_test = unit_test
+        self.nb_genes_kept = nb_genes_kept
+        # originally: GSE100866_CBMC_8K_13AB_10X-RNA_umi.csv.gz
+
+        if not self.unit_test:
+            self.download_name = 'cbmc.csv.gz'
+        else:
+            self.download_name = "../tests/data/cbmc_subsampled.h5"
+
         expression_data, gene_names = self.download_and_preprocess()
 
         super(CbmcDataset, self).__init__(
